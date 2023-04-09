6b942d226a443a9cf246ed5a71f7f41150fb0e5e
Mahdi Mzoughi <118602797+lmahdy@users.noreply.github.com>
2023-04-09 20:37:42 +0000

    Initial commit

--- script.js
+++ script.js
@@ -1,56 +1,58 @@
-const APILINK =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2b51354fa153eceb9be7c9a4c509d58f&page=1";
-const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI =
-  "https://api.themoviedb.org/3/search/movie?&api_key=2b51354fa153eceb9be7c9a4c509d58f&query=";
-const main = document.getElementById("section");
-const form = document.getElementById("form");
-const search = document.getElementById("query");
-returnMovies(APILINK);
-function returnMovies(url) {
-  fetch(url)
-    .then((res) => res.json())
-    .then(function (data) {
-      console.log(data.results);
-      const div_row = document.createElement("div");
-      div_row.setAttribute("class", "row");
-      data.results.forEach((element) => {
-        const div_card = document.createElement("div");
-        div_card.setAttribute("class", "card");
+  const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2b51354fa153eceb9be7c9a4c509d58f&page=1';
+  const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+  const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=2b51354fa153eceb9be7c9a4c509d58f&query=";
 
-        const div_column = document.createElement("div");
-        div_column.setAttribute("class", "column");
 
-        const image = document.createElement("img");
-        image.setAttribute("class", "thumbnail");
-        image.setAttribute("id", "image");
+  const main = document.getElementById("section");
+  const form = document.getElementById("form");
+  const search = document.getElementById("query");
 
-        const title = document.createElement("h3");
-        title.setAttribute("id", "title");
-
-        const center = document.createElement("center");
-
-        title.innerHTML = `${element.title}`;
+returnMovies(APILINK)
+  function returnMovies(url){
+    fetch(url).then(res => res.json())
+    .then(function(data){
+    console.log(data.results);
+    data.results.forEach(element => {
+        const div_card = document.createElement('div');
+        div_card.setAttribute('class', 'card');
+        
+        const div_row = document.createElement('div');
+        div_row.setAttribute('class', 'row');
+        
+        const div_column = document.createElement('div');
+        div_column.setAttribute('class', 'column');
+        
+        const image = document.createElement('img');
+        image.setAttribute('class', 'thumbnail');
+        image.setAttribute('id', 'image');
+        
+        const title = document.createElement('h3');
+        title.setAttribute('id', 'title');
+        
+        const center = document.createElement('center');
+  
+        title.innerHTML = `${element.title}<br><a href="movie.html?id=${element.id}&title=${element.title}">reviews</a>`;
         image.src = IMG_PATH + element.poster_path;
-
+  
         center.appendChild(image);
         div_card.appendChild(center);
         div_card.appendChild(title);
         div_column.appendChild(div_card);
         div_row.appendChild(div_column);
-      });
-      main.appendChild(div_row);
+  
+        main.appendChild(div_row);
     });
-}
+  });
+  }
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  main.innerHTML = "";
+  main.innerHTML = '';
 
   const searchItem = search.value;
 
   if (searchItem) {
     returnMovies(SEARCHAPI + searchItem);
-    search.value = "";
+      search.value = "";
   }
 });
